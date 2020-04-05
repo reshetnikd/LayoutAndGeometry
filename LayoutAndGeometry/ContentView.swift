d7f24adb483c7858fe690d8438eabe009544d4b3
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-06 00:12:52 +0300

    Try out some code with alignment guides

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -10,7 +10,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@nin")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("nin")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("TRENT REZNOR")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
+        }
+//        Text("Live long and prosper")
+//            .frame(width: 300, height: 300, alignment: .topLeading)
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+//        VStack(alignment: .leading) {
+//            Text("Hello, World!")
+//                .alignmentGuide(.leading) { d in d[.trailing] }
+//            Text("This is a longer line of text.")
+//        }
+//        .background(Color.red)
+//        .frame(width: 400, height: 400)
+//        .background(Color.blue)
+//        VStack(alignment: .leading) {
+//            ForEach(0..<10) { position in
+//                Text("Number \(position)")
+//                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10}
+//            }
+//        }
+//        .background(Color.red)
+//        .frame(width:400, height: 400)
+//        .background(Color.blue)
     }
 }
 
@@ -18,4 +61,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
